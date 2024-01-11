904dd548130037886e2df0c8b1b28065b34b4143
davidhui60 <49220706+davidhui60@users.noreply.github.com>
2024-01-10 19:09:51 -0500

    Add files via upload

---
+++ family/scripts/garllary-top.js
@@ -0,0 +1,8 @@
+const userName = 'david'
+// userName,title,comment
+let gallaryDb = ''
+gallaryDb += 'david,Alaska 2023'
+
+
+const files = fs.readdir(directory_path, callback_function)
+console.log(files)
